a256f8b7d131c3c68376d1f07bcd22428e738c6a
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-01-06 17:45:08 +0530

    feat: sidebar layout

--- 17-the-wild-oasis/solution/src/App.tsx
+++ 17-the-wild-oasis/solution/src/App.tsx
@@ -8,6 +8,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -15,13 +16,15 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />}></Route>
-          <Route path="dashboard" element={<Dashboard />}></Route>
-          <Route path="bookings" element={<Bookings />}></Route>
-          <Route path="cabins" element={<Cabins />}></Route>
-          <Route path="users" element={<Users />}></Route>
-          <Route path="settings" element={<Settings />}></Route>
-          <Route path="account" element={<Account />}></Route>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />}></Route>
+            <Route path="dashboard" element={<Dashboard />}></Route>
+            <Route path="bookings" element={<Bookings />}></Route>
+            <Route path="cabins" element={<Cabins />}></Route>
+            <Route path="users" element={<Users />}></Route>
+            <Route path="settings" element={<Settings />}></Route>
+            <Route path="account" element={<Account />}></Route>
+          </Route>
           <Route path="login" element={<Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
